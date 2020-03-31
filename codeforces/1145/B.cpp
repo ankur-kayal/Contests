f974d33bb05f3e97d98c03ceba39e4112e579389
Ankur Kayal <ankurkayal1234@gmail.com>
2020-03-31 13:23:00 +0530

    Add solution for problem `B - Kanban Numbers` Link: https://codeforces.com/contest/1145/problem/B Tags: brute force Ref: https://codeforces.com/contest/1145/submission/74898037

---
+++ codeforces/1145/B.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+ 
+int main() {
+    int n;
+    cin>>n;
+    if(n==2||n==3||n==4||n==5||n==12||n==30||n==35||n==43||n==52||n==64||n==86||n==46) cout<<"YES"<<endl;
+    else cout<<"NO"<<endl;
+	return 0;
+}
