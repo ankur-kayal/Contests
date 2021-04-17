d068ae9080c6fc8dfcc8b4b4c1c7594a97449752
Ankur Kayal <ankurkayal1234@gmail.com>
2021-04-17 21:57:00 +0530

    Add solution for problem `D - Points and Powers of Two` Link: https://codeforces.com/contest/988/problem/D Tags: brute force, math, *1800 Ref: https://codeforces.com/contest/988/submission/113349782

--- codeforces/988/D.cpp
+++ codeforces/988/D.cpp
@@ -1,45 +1,156 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
-int main() {
-#ifdef _DEBUG
-	freopen("input.txt", "r", stdin);
-//	freopen("output.txt", "w", stdout);
+//----------------------------------- DEBUG -----------------------------------
+#define sim template < class c
+#define ris return * this
+#define dor > debug & operator <<
+#define eni(x) sim > typename \
+enable_if<sizeof dud<c>(0) x 1, debug&>::type operator<<(c i) {
+sim > struct rge { c b, e; };
+sim > rge<c> range(c i, c j) { return rge<c>{i, j}; }
+sim > auto dud(c* x) -> decltype(cerr << *x, 0);
+sim > char dud(...);
+struct debug {
+#ifdef LOCAL
+~debug() { cerr << endl; }
+eni(!=) cerr << boolalpha << i; ris; }
+eni(==) ris << range(begin(i), end(i)); }
+sim, class b dor(pair < b, c > d) {
+  ris << "(" << d.first << ", " << d.second << ")";
+}
+sim dor(rge<c> d) {
+  *this << "[";
+  for (auto it = d.b; it != d.e; ++it)
+    *this << ", " + 2 * (it == d.b) << *it;
+  ris << "]";
+}
+#else
+sim dor(const c&) { ris; }
 #endif
-	
-	int n;
-	cin >> n;
-	vector<int> x(n);
-	for (int i = 0; i < n; ++i) {
-		cin >> x[i];
-	}
-	
-	sort(x.begin(), x.end());
-	vector<int> res = { x[0] };
-	
-	for (int i = 0; i < n; ++i) {
-		for (int j = 0; j < 31; ++j) {
-			int lx = x[i] - (1 << j);
-			int rx = x[i] + (1 << j);
-			bool isl = binary_search(x.begin(), x.end(), lx);
-			bool isr = binary_search(x.begin(), x.end(), rx);
-			if (isl && isr && int(res.size()) < 3) {
-				res = { lx, x[i], rx };
-			}
-			if (isl && int(res.size()) < 2) {
-				res = { lx, x[i] };
-			}
-			if (isr && int(res.size()) < 2) {
-				res = { x[i], rx };
-			}
-		}
-	}
-	
-	cout << int(res.size()) << endl;
-	for (auto it : res)
-		cout << it << " ";
-	cout << endl;
-	
-	return 0;
+};
+#define imie(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
+// debug & operator << (debug & dd, P p) { dd << "(" << p.x << ", " << p.y << ")"; return dd; }
+
+//----------------------------------- END DEBUG --------------------------------
+
+// http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2016/p0200r0.html
+template<class Fun> class y_combinator_result {
+    Fun fun_;
+public:
+    template<class T> explicit y_combinator_result(T &&fun): fun_(std::forward<T>(fun)) {}
+    template<class ...Args> decltype(auto) operator()(Args &&...args) { return fun_(std::ref(*this), std::forward<Args>(args)...); }
+};
+template<class Fun> decltype(auto) y_combinator(Fun &&fun) { return y_combinator_result<std::decay_t<Fun>>(std::forward<Fun>(fun)); }
+ 
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+#define trav(a,x) for (auto& a : x)
+#define uid(a, b) uniform_int_distribution<int>(a, b)(rng)
+
+//----------------------------------- DEFINES ----------------------------------- 
+
+#define sz(x) (int)(x).size()
+#define mp make_pair
+#define eb emplace_back
+#define pb push_back
+#define lb lower_bound
+#define ub upper_bound
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define ins insert
+#define nl '\n'
+#define Stringize( L )     #L 
+#define MakeString( M, L ) M(L)
+#define $Line MakeString( Stringize, __LINE__ )
+#define Reminder __FILE__ "("  ") : Warning: "
+
+//----------------------------------- END DEFINES -------------------------------- 
+
+//-------------------------- CUSTOM UNORDERED MAP HASH ---------------------------
+
+struct custom_hash{
+    static uint64_t splitmix64(uint64_t x){
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+    size_t operator()(uint64_t a) const {
+        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(a + FIXED_RANDOM);
+    }
+    template<class T> size_t operator()(T a) const {
+        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+        hash<T> x;
+        return splitmix64(x(a) + FIXED_RANDOM);
+    }
+    template<class T, class H> size_t operator()(pair<T, H> a) const {
+        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+        hash<T> x;
+        hash<H> y;
+        return splitmix64(x(a.first) * 37 + y(a.second) + FIXED_RANDOM);
+    }
+};
+template<class T, class H>using umap=unordered_map<T,H,custom_hash>;
+
+//----------------------- CUSTOM UNORDERED MAP HASH END--------------------------
+
+void run_cases() {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+
+    trav(u, a) cin >> u;
+
+    set<int> uni(all(a));
+
+    for(int i=0;i<n;i++) {
+        for(int j=0;j<31;j++) {
+            int pow2 = 1 << j;
+            if(uni.count(a[i] - pow2) and uni.count(a[i] + pow2)) {
+                cout << 3 << nl;
+                cout << a[i] << " " << a[i] - pow2 << " " << a[i] + pow2 << nl;
+                return;
+            }
+        }
+    }
+
+    for(int i=0;i<n;i++) {
+        for(int j=0;j<31;j++) {
+            int pow2 = 1 << j;
+            if(uni.count(a[i] - pow2)) {
+                cout << 2 << nl;
+                cout << a[i] << " " << a[i] - pow2 << nl;
+                return;
+            }
+        }
+    }
+
+    for(int i=0;i<n;i++) {
+        for(int j=0;j<31;j++) {
+            int pow2 = 1 << j;
+            if(uni.count(a[i] + pow2)) {
+                cout << 2 << nl;
+                cout << a[i] << " " << a[i] + pow2 << nl;
+                return;
+            }
+        }
+    }
+
+    cout << 1 << nl;
+    cout << a[0] << nl;
+
+}
+
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(nullptr);
+
+    int tests = 1;
+    // cin >> tests;
+
+    for(int test = 1;test <= tests;test++) {
+        run_cases();
+    }
 }
