adb91cc92db93e50c796ac9f76001f2cbad66375
Ankur Kayal <ankurkayal1234@gmail.com>
2021-04-20 00:21:00 +0530

    Add solution for problem `D - Cut and Stick` Link: https://codeforces.com/contest/1514/problem/D Tags: binary search, data structures, greedy, implementation, sortings, *2000 Ref: https://codeforces.com/contest/1514/submission/113551140

--- codeforces/1514/D.cpp
+++ codeforces/1514/D.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -97,203 +98,30 @@ template<class T, class H>using umap=unordered_map<T,H,custom_hash>;
 
 //----------------------- CUSTOM UNORDERED MAP HASH END--------------------------
 
-struct custom_node {
-    int64_t val;
-    int64_t count;
-    int64_t lazy;
-    // add more data types if you need  
-
-    custom_node() {
-        // ... init here with default neutral values;
-
-        
-        val = 0;
-        count = 0;
-        lazy = -1;
-        
-    }
-
-    custom_node(int64_t _val) {
-        // ... init here with the actual value
-        
-        val = _val;
-        count = 1;
-        lazy = -1;
-        
-    }
-
-    custom_node op(custom_node& other) {
-        // ... update the operation you want to use
-        
-        custom_node ans;
-        if(val == other.val) {
-            ans.count = count + other.count;
-            ans.val = val;
-        } else if(count > other.count) {
-            ans.val = val;
-            ans.count = count - other.count;
-        } else if(count < other.count) {
-            ans.val = other.val;
-            ans.count = other.count - count;
-        }
-        return ans;
-        
-    }
-
-    custom_node lazy_op(int64_t val, int64_t size) {
-        // ... define the lazy operation you want to use
-        
-        custom_node ans;
-        ans.lazy = val;
-        ans.val = (val);
-        ans.count = 1;
-        return ans;
-        
-    }
-};
-
-debug & operator << (debug & dd, custom_node p) { dd << "(" << p.val << ", " << p.count << ")"; return dd; }
-
-struct segment_tree {
-    int size; // size of the segment tree, 
-
-    custom_node NEUTRAL_ELEMENT; //  neutral element
-
-    int NO_LAZY; // define the parameter which defines if you have any lazy left
-
-    vector<custom_node> values; // segment tree cumulative values
-
-    void init(int n) {
-        // #pragma message(Reminder "Don't forget to define the lazy parameter")
-        NO_LAZY = -1; // define the no lazy parameter
-        size = 1;
-        while(size < n) {
-            size *= 2;
-        }
-        values.assign(2 * size, NEUTRAL_ELEMENT);
-    }
-
-    custom_node merge(custom_node& a, custom_node& b) {
-        custom_node res;
-        res = a.op(b);
-        return res;
-    }
-
-    void build(vector<int64_t>& a, int x, int lx, int rx) {
-        if(rx - lx == 1) {
-            if(lx < int(a.size())) {
-                values[x] = custom_node(a[lx]);
-            }
-            return;
-        }
-
-        int m = (rx + lx) / 2;
-        build(a, 2 * x + 1, lx, m);
-        build(a, 2 * x + 2, m, rx);
-        values[x] = merge(values[2 * x + 1], values[2 * x + 2]);
-    }
-
-    void build(vector<int64_t>& a) {
-        build(a, 0, 0, size);
-    }
-
-    custom_node query(int l, int r, int x, int lx, int rx) {
-        propagate(x, lx, rx);
-        if(lx >= r or l >= rx) {
-            return NEUTRAL_ELEMENT;
-        }
-
-        else if(lx >= l and rx <= r) {
-            return values[x];
-        }
-
-        int m = (lx + rx) / 2;
-
-        custom_node s1 = query(l, r, 2 * x + 1, lx, m);
-        custom_node s2 = query(l, r, 2 * x + 2, m, rx);
-        return merge(s1, s2);
-    }
-
-    custom_node query(int l, int r) {
-        return query(l, r, 0, 0, size);
-    }
-
-    void propagate(int x, int lx, int rx) {
-        if(values[x].lazy == NO_LAZY) {
-            return;
-        }
-        int m = (rx - lx) / 2;
-
-        values[x] = values[x].lazy_op(values[x].lazy, rx - lx);
-
-        if(rx - lx != 1) { // if you have children propagate the lazy parameter to the children
-            // custom define how you want to update !! Most errors occur Here !!
-            // #pragma message(Reminder "Define how you want to pass lazy parameters, once done comment this")
-            values[2 * x + 1].lazy = values[x].lazy;
-            values[2 * x + 2].lazy = values[x].lazy;
-
-        }
-
-        values[x].lazy = NO_LAZY;
-    }
-
-    void range_update(int l, int r, int val, int x, int lx, int rx) {
-        propagate(x, lx, rx); // try propagating once you touch every node
-        if(lx >= r or l >= rx) {
-            return;
-        }
-        if(lx >= l and rx <= r) {
-            // custom define how you want to update !! Most errors occur Here !!
-            // #pragma message(Reminder "Define how you want to pass lazy parameters and update, once done comment this")
-            // values[x].val = val;
-            values[x].lazy = val;
-            propagate(x, lx, rx);
-            return;
-        }
-
-        int m = (lx + rx) / 2;
-        range_update(l, r, val, 2 * x + 1, lx, m);
-        range_update(l, r, val, 2 * x + 2, m, rx);
-
-        values[x] = merge(values[2 * x + 1], values[2 * x + 2]);
-    }
-
-    void range_update(int l, int r, int val) {
-        range_update(l, r, val, 0, 0, size);
-    }
-};
-
 void run_cases() {
     int N,Q;
     cin >> N >> Q;
-    vector<int64_t> A(N);
+    vector<int> A(N);
+
     trav(u, A) cin >> u;
 
-
-    segment_tree tree;
-    tree.init(N);
-    tree.build(A);
-
-    vector<vector<int>> locations(N + 1);
+    vector<vector<int>> segments(N + 1);
     for(int i=0;i<N;i++) {
-        locations[A[i]].push_back(i + 1);
+        segments[A[i]].push_back(i + 1);
     }
 
     while(Q--) {
-        int l, r;
+        int l,r;
         cin >> l >> r;
         int segment_length = r - l + 1;
-        l--;
-        int val = tree.query(l, r).val;
-        if(val == 0) {
-            cout << 1 << nl;
-            continue;
-        }
-
         int ans = 1;
         int freq;
-        freq = upper_bound(all(locations[val]), r)  - lower_bound(all(locations[val]), l + 1);
-        ans = max(ans, 2 * freq - segment_length);
+        for(int i=0;i<30;i++) {
+            int index = uid(l-1, r-1);
+
+            freq = upper_bound(all(segments[A[index]]), r)  - lower_bound(all(segments[A[index]]), l);
+            ans = max(ans, 2 * freq - segment_length);
+        }
         cout << ans << nl;
     }
 }
