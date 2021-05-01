9d2b41d38527bf31ef1578b38f553f3929221ef3
Ankur Kayal <ankurkayal1234@gmail.com>
2021-05-01 19:49:00 +0530

    Add solution for problem `D - Pashmak and Parmida's problem` Link: https://codeforces.com/contest/459/problem/D Tags: data structures, divide and conquer, sortings, *1800 Ref: https://codeforces.com/contest/459/submission/114781172

---
+++ codeforces/459/D.cpp
@@ -0,0 +1,262 @@
+#include <algorithm>
+#include <bits/stdc++.h>
+using namespace std;
+
+//----------------------------------- DEBUG -----------------------------------
+#define sim template < class c
+#define ris return * this
+#define dor > debug & operator <<
+#define eni(x) sim > typename \
+enable_if<sizeof dud<c>(0) x 1, debug&>::type operator<<(c i) {
+sim > struct rge { c b, e; };
+sim > rge<c> range(c i, c j) { return rge<c>{i, j}; }
+sim > auto dud(c* x) -> decltype(cerr << *x, 0);
+sim > char dud(...);
+struct debug {
+#ifdef LOCAL
+~debug() { cerr << endl; }
+eni(!=) cerr << boolalpha << i; ris; }
+eni(==) ris << range(begin(i), end(i)); }
+sim, class b dor(pair < b, c > d) {
+  ris << "(" << d.first << ", " << d.second << ")";
+}
+sim dor(rge<c> d) {
+  *this << "[";
+  for (auto it = d.b; it != d.e; ++it)
+    *this << ", " + 2 * (it == d.b) << *it;
+  ris << "]";
+}
+#else
+sim dor(const c&) { ris; }
+#endif
+};
+#define imie(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
+// debug & operator << (debug & dd, P p) { dd << "(" << p.x << ", " << p.y << ")"; return dd; }
+
+//----------------------------------- END DEBUG --------------------------------
+
+// http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2016/p0200r0.html
+template<class Fun> class y_combinator_result {
+    Fun fun_;
+public:
+    template<class T> explicit y_combinator_result(T &&fun): fun_(std::forward<T>(fun)) {}
+    template<class ...Args> decltype(auto) operator()(Args &&...args) { return fun_(std::ref(*this), std::forward<Args>(args)...); }
+};
+template<class Fun> decltype(auto) y_combinator(Fun &&fun) { return y_combinator_result<std::decay_t<Fun>>(std::forward<Fun>(fun)); }
+ 
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+#define trav(a,x) for (auto& a : x)
+#define uid(a, b) uniform_int_distribution<int>(a, b)(rng)
+
+//----------------------------------- DEFINES ----------------------------------- 
+
+#define sz(x) (int)(x).size()
+#define mp make_pair
+#define eb emplace_back
+#define pb push_back
+#define lb lower_bound
+#define ub upper_bound
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define ins insert
+#define nl '\n'
+#define Stringize( L )     #L 
+#define MakeString( M, L ) M(L)
+#define $Line MakeString( Stringize, __LINE__ )
+#define Reminder __FILE__ "("  ") : Warning: "
+
+//----------------------------------- END DEFINES -------------------------------- 
+
+//-------------------------- CUSTOM UNORDERED MAP HASH ---------------------------
+
+struct custom_hash{
+    static uint64_t splitmix64(uint64_t x){
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+    size_t operator()(uint64_t a) const {
+        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(a + FIXED_RANDOM);
+    }
+    template<class T> size_t operator()(T a) const {
+        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+        hash<T> x;
+        return splitmix64(x(a) + FIXED_RANDOM);
+    }
+    template<class T, class H> size_t operator()(pair<T, H> a) const {
+        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+        hash<T> x;
+        hash<H> y;
+        return splitmix64(x(a.first) * 37 + y(a.second) + FIXED_RANDOM);
+    }
+};
+template<class T, class H>using umap=unordered_map<T,H,custom_hash>;
+
+//----------------------- CUSTOM UNORDERED MAP HASH END--------------------------
+
+template<typename T>
+struct fenwick_tree {
+    int tree_n = 0;
+    T tree_sum = 0;
+    vector<T> tree;
+ 
+    fenwick_tree(int n = -1) {
+        if (n >= 0)
+            init(n);
+    }
+ 
+    static int highest_bit(int x) {
+        return x == 0 ? -1 : 31 - __builtin_clz(x);
+    }
+ 
+    void init(int n) {
+        tree_n = n;
+        tree_sum = 0;
+        tree.assign(tree_n + 1, 0);
+    }
+ 
+    // O(n) initialization of the Fenwick tree.
+    template<typename T_array>
+    void build(const T_array &initial) {
+        assert(int(initial.size()) == tree_n);
+        tree_sum = 0;
+ 
+        for (int i = 1; i <= tree_n; i++) {
+            tree[i] = initial[i - 1];
+            tree_sum += initial[i - 1];
+ 
+            for (int k = (i & -i) >> 1; k > 0; k >>= 1)
+                tree[i] += tree[i - k];
+        }
+    }
+ 
+    // index is in [0, tree_n).
+    void update(int index, const T &change) {
+        assert(0 <= index && index < tree_n);
+        tree_sum += change;
+ 
+        for (int i = index + 1; i <= tree_n; i += i & -i)
+            tree[i] += change;
+    }
+ 
+    // Returns the sum of the range [0, count).
+    T query(int count) const {
+        count = min(count, tree_n);
+        T sum = 0;
+ 
+        for (int i = count; i > 0; i -= i & -i)
+            sum += tree[i];
+ 
+        return sum;
+    }
+ 
+    // Returns the sum of the range [start, tree_n).
+    T query_suffix(int start) const {
+        return tree_sum - query(start);
+    }
+ 
+    // Returns the sum of the range [a, b).
+    T query(int a, int b) const {
+        return query(b) - query(a);
+    }
+ 
+    // Returns the element at index a in O(1) amortized across every index. Equivalent to query(a, a + 1).
+    T get(int a) const {
+        assert(0 <= a && a < tree_n);
+        int above = a + 1;
+        T sum = tree[above];
+        above -= above & -above;
+ 
+        while (a != above) {
+            sum -= tree[a];
+            a -= a & -a;
+        }
+ 
+        return sum;
+    }
+ 
+    bool set(int index, T value) {
+        assert(0 <= index && index < tree_n);
+        T current = get(index);
+ 
+        if (current == value)
+            return false;
+ 
+        update(index, value - current);
+        return true;
+    }
+ 
+    // Returns the largest p in `[0, tree_n]` such that `query(p) <= sum`. Returns -1 if no such p exists (`sum < 0`).
+    // Can be used as an ordered set/multiset on indices in `[0, tree_n)` by using the tree as a 0/1 or frequency array:
+    // `set(index, 1)` is equivalent to insert(index). `update(index, +1)` is equivalent to multiset.insert(index).
+    // `set(index, 0)` or `update(index, -1)` are equivalent to erase(index).
+    // `get(index)` provides whether index is present or not, or the count of index (if multiset).
+    // `query(index)` provides the count of elements < index.
+    // `find_last_prefix(k)` finds the k-th smallest element (0-indexed). Returns `tree_n` for `sum >= set.size()`.
+    int find_last_prefix(T sum) const {
+        if (sum < 0)
+            return -1;
+ 
+        int prefix = 0;
+ 
+        for (int k = highest_bit(tree_n); k >= 0; k--)
+            if (prefix + (1 << k) <= tree_n && tree[prefix + (1 << k)] <= sum) {
+                prefix += 1 << k;
+                sum -= tree[prefix];
+            }
+ 
+        return prefix;
+    }
+};
+
+void run_cases() {
+    int n;
+    cin >> n;
+    fenwick_tree<int64_t> left_tree, right_tree;
+    left_tree.init(n + 10); right_tree.init(n + 10);
+    map<int,int> compress;
+    vector<int> a(n);
+    trav(u, a) cin >> u;
+    vector<pair<int, int>> pairs(n);
+    for(int i = 0; i < n; ++i) {
+        pairs[i] = {a[i], i};
+    }
+    sort(pairs.begin(), pairs.end());
+    int nxt = 1;
+    for(int i = 0; i < n; ++i) {
+        if(i > 0 && pairs[i-1].first != pairs[i].first) nxt++;
+        a[pairs[i].second] = nxt;
+    }
+
+    int64_t ans = 0;
+
+    vector<int> prefix(n + 10), suffix(n + 10);
+    for(int i=n-1;i>=0;i--) {
+        suffix[a[i]]++;
+        right_tree.update(suffix[a[i]], 1);
+    }
+
+    for(int i=0;i<n-1;i++) {
+        prefix[a[i]]++;
+        left_tree.update(prefix[a[i]], 1);
+        right_tree.update(suffix[a[i]], -1);
+        suffix[a[i]]--;
+        ans += right_tree.query(0, prefix[a[i]]);
+    }
+    cout << ans;
+}
+
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(nullptr);
+
+    int tests = 1;
+    // cin >> tests;
+
+    for(int test = 1;test <= tests;test++) {
+        run_cases();
+    }
+}
