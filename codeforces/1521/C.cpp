fb1f0489504377027000a093b6e048448a8dce5e
Ankur Kayal <ankurkayal1234@gmail.com>
2021-05-08 00:04:00 +0530

    Add solution for problem `C - Nastia and a Hidden Permutation` Link: https://codeforces.com/contest/1521/problem/C Tags: constructive algorithms, interactive, *2000 Ref: https://codeforces.com/contest/1521/submission/115628277

--- codeforces/1521/C.cpp
+++ codeforces/1521/C.cpp
@@ -1,10 +1,44 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
+//----------------------------------- DEBUG -----------------------------------
+#define sim template < class c
+#define ris return * this
+#define dor > debug & operator <<
+#define eni(x) sim > typename \
+enable_if<sizeof dud<c>(0) x 1, debug&>::type operator<<(c i) {
+sim > struct rge { c b, e; };
+sim > rge<c> range(c i, c j) { return rge<c>{i, j}; }
+sim > auto dud(c* x) -> decltype(cerr << *x, 0);
+sim > char dud(...);
+struct debug {
+#ifdef LOCAL
+~debug() { cerr << endl; }
+eni(!=) cerr << boolalpha << i; ris; }
+eni(==) ris << range(begin(i), end(i)); }
+sim, class b dor(pair < b, c > d) {
+  ris << "(" << d.first << ", " << d.second << ")";
+}
+sim dor(rge<c> d) {
+  *this << "[";
+  for (auto it = d.b; it != d.e; ++it)
+    *this << ", " + 2 * (it == d.b) << *it;
+  ris << "]";
+}
+#else
+sim dor(const c&) { ris; }
+#endif
+};
+#define imie(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
+// debug & operator << (debug & dd, P p) { dd << "(" << p.x << ", " << p.y << ")"; return dd; }
+
+//----------------------------------- END DEBUG --------------------------------
+
 #define nl '\n'
 
 int query(int t, int i, int j, int x) {
-    cout << "? " << t << " " << i << " " << j << " " << x << '\n' << flush;
+    cout << "? " << t << " " << i << " " << j << " " << x << endl;
     int val;
     cin >> val;
     if(val == -1) {
@@ -18,7 +52,8 @@ void output(vector<int> a) {
     for(auto u: a) {
         cout << u << " ";
     }
-    cout << '\n' << flush;
+    cout << endl;
+    int verdict;
     
 }
 
