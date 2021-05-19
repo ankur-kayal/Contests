55d68963fcb266572d38d88b246289931f7aeba6
Ankur Kayal <ankurkayal1234@gmail.com>
2021-05-19 12:55:00 +0530

    Add solution for problem `E2 - Weights Division (hard version)` Link: https://codeforces.com/contest/1399/problem/E2 Tags: binary search, dfs and similar, greedy, sortings, trees, two pointers, *2200 Ref: https://codeforces.com/contest/1399/submission/116626087

--- codeforces/1399/E2.cpp
+++ codeforces/1399/E2.cpp
@@ -11,6 +11,39 @@ public:
 };
 template<class Fun> decltype(auto) y_combinator(Fun &&fun) { return y_combinator_result<std::decay_t<Fun>>(std::forward<Fun>(fun)); }
 
+
+//----------------------------------- DEBUG -----------------------------------
+#define sim template < class c
+#define ris return * this
+#define dor > debug & operator <<
+#define eni(x) sim > typename \
+enable_if<sizeof dud<c>(0) x 1, debug&>::type operator<<(c i) {
+sim > struct rge { c b, e; };
+sim > rge<c> range(c i, c j) { return rge<c>{i, j}; }
+sim > auto dud(c* x) -> decltype(cerr << *x, 0);
+sim > char dud(...);
+struct debug {
+#ifdef LOCAL
+~debug() { cerr << endl; }
+eni(!=) cerr << boolalpha << i; ris; }
+eni(==) ris << range(begin(i), end(i)); }
+sim, class b dor(pair < b, c > d) {
+  ris << "(" << d.first << ", " << d.second << ")";
+}
+sim dor(rge<c> d) {
+  *this << "[";
+  for (auto it = d.b; it != d.e; ++it)
+    *this << ", " + 2 * (it == d.b) << *it;
+  ris << "]";
+}
+#else
+sim dor(const c&) { ris; }
+#endif
+};
+#define imie(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
+// debug & operator << (debug & dd, P p) { dd << "(" << p.x << ", " << p.y << ")"; return dd; }
+
+//----------------------------------- END DEBUG --------------------------------
 void run_cases() {
     int64_t N, S;
     cin >> N >> S;
@@ -75,6 +108,14 @@ void run_cases() {
     for(auto u: order2) {
         prefix2.push_back(prefix2.back() + u);
     }
+
+    // prefix1.erase(prefix1.begin());
+    // prefix2.erase(prefix2.begin());
+
+    // reverse(prefix1.begin(), prefix1.end());
+    // reverse(prefix2.begin(), prefix2.end());
+
+    debug() << imie(prefix1)  << '\n' << imie(prefix2);
 
     if(path_sum <= S) {
         cout << 0 << '\n';
