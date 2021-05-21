0cb4dfd1f2701b43ad0f67ea0dc23caa14859d63
Ankur Kayal <ankurkayal1234@gmail.com>
2021-05-22 01:20:00 +0530

    Add solution for problem `A - And Then There Were K` Link: https://codeforces.com/contest/1527/problem/A Tags: bitmasks, *800 Ref: https://codeforces.com/contest/1527/submission/116955904

---
+++ codeforces/1527/A.rs
@@ -0,0 +1,34 @@
+use std::io::{stdin, stdout, BufWriter, Write};
+#[derive(Default)]
+struct Scanner {
+  buffer: Vec<String>,
+}
+impl Scanner {
+  fn next<T: std::str::FromStr>(&mut self) -> T {
+    loop {
+      if let Some(token) = self.buffer.pop() {
+        return token.parse().ok().expect("Failed parse");
+      }
+      let mut input = String::new();
+      stdin().read_line(&mut input).expect("Failed read");
+      self.buffer = input.split_whitespace().rev().map(String::from).collect();
+    }
+  }
+}
+
+fn main() {
+  let mut scan = Scanner::default();
+  let out = &mut BufWriter::new(stdout());
+
+  let t: i32 = scan.next();
+  for _ in 0..t {
+    let n: i64 = scan.next();
+    for i in 0..45 {
+      let k: i64 = (1i64 << i) - 1;
+      if k >= n {
+        writeln!(out, "{}", k >> 1).ok();
+        break;
+      }
+    }
+  }
+}
