1ca6f0893b275acf9943f2a7c3296908b62c3fc5
Ankur Kayal <ankurkayal1234@gmail.com>
2021-05-22 21:16:00 +0530

    Add solution for problem `D - MEX Tree` Link: https://codeforces.com/contest/1527/problem/D Tags: combinatorics, dfs and similar, implementation, math, trees, *2400 Ref: https://codeforces.com/contest/1527/submission/117029709

--- codeforces/1527/D.cpp
+++ codeforces/1527/D.cpp
@@ -3,6 +3,40 @@ using namespace std;
 
 #define nl '\n'
 
+
+//----------------------------------- DEBUG -----------------------------------
+#define sim template < class c
+#define ris return * this
+#define dor > debug & operator <<
+#define eni(x) sim > typename \
+enable_if<sizeof dud<c>(0) x 1, debug&>::type operator<<(c i) {
+sim > struct rge { c b, e; };
+sim > rge<c> range(c i, c j) { return rge<c>{i, j}; }
+sim > auto dud(c* x) -> decltype(cerr << *x, 0);
+sim > char dud(...);
+struct debug {
+#ifdef LOCAL
+~debug() { cerr << endl; }
+eni(!=) cerr << boolalpha << i; ris; }
+eni(==) ris << range(begin(i), end(i)); }
+sim, class b dor(pair < b, c > d) {
+  ris << "(" << d.first << ", " << d.second << ")";
+}
+sim dor(rge<c> d) {
+  *this << "[";
+  for (auto it = d.b; it != d.e; ++it)
+    *this << ", " + 2 * (it == d.b) << *it;
+  ris << "]";
+}
+#else
+sim dor(const c&) { ris; }
+#endif
+};
+#define imie(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
+// debug & operator << (debug & dd, P p) { dd << "(" << p.x << ", " << p.y << ")"; return dd; }
+
+//----------------------------------- END DEBUG --------------------------------
+
 template<typename T_vector>
 void output_vector(const T_vector &v, bool add_one = false, int start = -1, int end = -1) {
     if (start < 0) start = 0;
@@ -71,6 +105,8 @@ void run_cases() {
             u = parent[u];
         }
 
+        debug() << imie(u);
+
         if(u == l || u == r) {
             if(u == l) {
                 l = i;
@@ -80,6 +116,7 @@ void run_cases() {
 
             ans[i] = P - subtree[l] * subtree[r];
             P = subtree[l] * subtree[r];
+            debug() << imie(l) imie(r);
         } else {
             ans[i] = P;
             P = 0;
