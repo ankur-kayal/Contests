993c04cca8683f6a5f0e4ce7aecbcd9dc0dfe7dc
Ankur Kayal <ankurkayal1234@gmail.com>
2021-06-14 23:44:00 +0530

    Add solution for problem `C - Little Alawn's Puzzle` Link: https://codeforces.com/contest/1534/problem/C Tags: combinatorics, dp, dsu, graphs, math Ref: https://codeforces.com/contest/1534/submission/119480138

--- codeforces/1534/C.py
+++ codeforces/1534/C.py
@@ -1,49 +1,34 @@
-def main():
-    def input(): return sys.stdin.readline().strip("\n")
-    def I():    return (input())
-    def II():    return (int(input()))
-    def MI():    return (map(int,input().split()))
-    def LI():    return (list(map(int,input().split())))
-    def isPerfectSquare(x): return (int(math.sqrt(x))**2 == x)
-    sys.setrecursionlimit(500000)
-    from collections import OrderedDict
-    MOD = 10**9 + 7
+import sys
+import math
+import heapq
+def input(): return sys.stdin.readline().strip("\n")
+def I():    return (input())
+def II():    return (int(input()))
+def MI():    return (map(int,input().split()))
+def LI():    return (list(map(int,input().split())))
+def isPerfectSquare(x): return (int(math.sqrt(x))**2 == x)
+sys.setrecursionlimit(500000)
+from collections import OrderedDict
+MOD = 10**9 + 7
+
+for _ in range(II()):
+    graph = {}
     def dfs(x):
         if not visited[x]:
             visited[x] = 1
             dfs(graph[x])
-    for _ in range(II()):
-        n = II()
-        a = LI()
-        b = LI()
-        graph = {}
-        visited = [0]*(n+1)
-        for i in range(n):
-            graph[a[i]] = b[i]
-        ans = 1
-        for i in range(n):
-            if visited[a[i]] == 0:
-                dfs(a[i])
-                ans = (ans * 2) % MOD
-        print(ans)
-
-
-if __name__ == "__main__":
-    import sys, threading
-    import bisect
-    import math
-    import itertools
-    from sys import stdout
- 
-    ############  Sorted Containers  ######################
-    import heapq
-    from queue import PriorityQueue
- 
-    ############ Tree Problems ( Use Python 3) ###########
-    sys.setrecursionlimit(2 ** 32 // 2 - 1)
-    threading.stack_size(1 << 27)
- 
-    input = sys.stdin.readline
-    thread = threading.Thread(target=main)
-    thread.start()
-    thread.join()
+    n = II()
+    a = LI()
+    b = LI()
+    visited = [0]*(n+1)
+    for i in range(n):
+        graph[a[i]] = b[i]
+    ans = 1
+    for i in range(n):
+        if visited[a[i]] == 0:
+            nxt = a[i]
+            while visited[nxt] == 0:
+                visited[nxt] = 1
+                nxt = graph[nxt]
+            ans = (ans * 2) % MOD
+    print(ans)
