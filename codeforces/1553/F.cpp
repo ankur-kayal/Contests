0df3b4338bb490de3d5b2a7083b203207ae8907c
Ankur Kayal <ankurkayal1234@gmail.com>
2021-07-23 02:35:00 +0530

    Add solution for problem `F - Pairwise Modulo` Link: https://codeforces.com/contest/1553/problem/F Tags: data structures, math, number theory, *2300 Ref: https://codeforces.com/contest/1553/submission/123367630

--- codeforces/1553/F.cpp
+++ codeforces/1553/F.cpp
@@ -4,118 +4,236 @@ using namespace std;
 
 #define nl '\n'
 
-template<typename T>
-struct fenwick_tree {
+struct segment_change {
+    int64_t to_add;
+    static const int64_t SENTINEL = 0;
+
+    // Make sure that the default constructor is the identity segment_change
+    segment_change(int64_t _to_add = SENTINEL) : to_add(_to_add) {}
+
+    // resets to the SENTINEL value
+    void reset() {
+        to_add = SENTINEL;
+    }
+
+    bool has_change() const {
+        return to_add != SENTINEL;
+    }
+
+    // Return the combined result of applying this segment change followed by the `other`.
+    segment_change combine(const segment_change &other) const {
+        return segment_change(to_add + other.to_add);
+    }
+};
+
+struct segment {
+    int64_t sum;
+
+    // Make sure that the default constructor is the identity segment
+    segment(int64_t _sum = 0) : sum(_sum) {}
+
+    // checks if the current segment is empty based on the default value
+    bool empty() const {
+        return sum == 0;
+    }
+
+    // evaluating the change on a particular node
+    void apply(int length, const segment_change &change) {
+        if(change.has_change()) {
+            sum = sum + change.to_add * length;
+        }
+    }
+
+    // merge function
+    void join(const segment &other) {
+        sum = sum + other.sum;
+    }
+
+    void join(const segment &a, const segment &b) {
+        *this = a;
+        join(b);
+    }
+};
+
+int highest_bit(int x) {
+    return x == 0 ? -1 : 31 - __builtin_clz(x);
+}
+
+pair<int, int> right_half[32];
+
+struct segment_tree {
     int tree_n = 0;
-    T tree_sum = 0;
-    vector<T> tree;
- 
-    fenwick_tree(int n = -1) {
+    vector<segment> tree;
+    vector<segment_change> changes;
+
+    segment_tree(int n = -1) {
         if (n >= 0)
             init(n);
     }
- 
-    static int highest_bit(int x) {
-        return x == 0 ? -1 : 31 - __builtin_clz(x);
-    }
- 
+
     void init(int n) {
-        tree_n = n;
-        tree_sum = 0;
-        tree.assign(tree_n + 1, 0);
+        tree_n = 1;
+
+        while (tree_n < n)
+            tree_n *= 2;
+
+        tree.assign(2 * tree_n, segment());
+        changes.assign(tree_n, segment_change());
     }
- 
-    // O(n) initialization of the Fenwick tree.
-    template<typename T_array>
-    void build(const T_array &initial) {
-        assert(int(initial.size()) == tree_n);
-        tree_sum = 0;
- 
-        for (int i = 1; i <= tree_n; i++) {
-            tree[i] = initial[i - 1];
-            tree_sum += initial[i - 1];
- 
-            for (int k = (i & -i) >> 1; k > 0; k >>= 1)
-                tree[i] += tree[i - k];
+
+    // Builds our tree from an array in O(n).
+    void build(const vector<segment> &initial) {
+        int n = int(initial.size());
+        init(n);
+        assert(n <= tree_n);
+
+        for (int i = 0; i < n; i++)
+            tree[tree_n + i] = initial[i];
+
+        for (int position = tree_n - 1; position > 0; position--)
+            tree[position].join(tree[2 * position], tree[2 * position + 1]);
+    }
+
+    void apply_and_combine(int position, int length, const segment_change &change) {
+        tree[position].apply(length, change);
+        if (position < tree_n) changes[position] = changes[position].combine(change);
+    }
+
+    void push_down(int position, int length) {
+        if (changes[position].has_change()) {
+            apply_and_combine(2 * position, length / 2, changes[position]);
+            apply_and_combine(2 * position + 1, length / 2, changes[position]);
+            changes[position].reset();
         }
     }
- 
-    // index is in [0, tree_n).
-    void update(int index, const T &change) {
-        assert(0 <= index && index < tree_n);
-        tree_sum += change;
- 
-        for (int i = index + 1; i <= tree_n; i += i & -i)
-            tree[i] += change;
-    }
- 
-    // Returns the sum of the range [0, count).
-    T query(int count) const {
-        count = min(count, tree_n);
-        T sum = 0;
- 
-        for (int i = count; i > 0; i -= i & -i)
-            sum += tree[i];
- 
-        return sum;
-    }
- 
-    // Returns the sum of the range [start, tree_n).
-    T query_suffix(int start) const {
-        return tree_sum - query(start);
-    }
- 
-    // Returns the sum of the range [a, b).
-    T query(int a, int b) const {
-        return query(b) - query(a);
-    }
- 
-    // Returns the element at index a in O(1) amortized across every index. Equivalent to query(a, a + 1).
-    T get(int a) const {
-        assert(0 <= a && a < tree_n);
-        int above = a + 1;
-        T sum = tree[above];
-        above -= above & -above;
- 
-        while (a != above) {
-            sum -= tree[a];
-            a -= a & -a;
+
+    // Calls push_down for all necessary nodes in order to query the range [a, b).
+    void push_all(int a, int b) {
+        assert(0 <= a && a < b && b <= tree_n);
+        a += tree_n;
+        b += tree_n - 1;
+
+        for (int up = highest_bit(tree_n); up > 0; up--) {
+            int x = a >> up, y = b >> up;
+            push_down(x, 1 << up);
+            if (x != y) push_down(y, 1 << up);
         }
- 
-        return sum;
     }
- 
-    bool set(int index, T value) {
+
+    void join_and_apply(int position, int length) {
+        tree[position].join(tree[2 * position], tree[2 * position + 1]);
+        tree[position].apply(length, changes[position]);
+    }
+
+    // Calls join for all necessary nodes after updating the range [a, b).
+    void join_all(int a, int b) {
+        assert(0 <= a && a < b && b <= tree_n);
+        a += tree_n;
+        b += tree_n - 1;
+        int length = 1;
+
+        while (a > 1) {
+            a /= 2;
+            b /= 2;
+            length *= 2;
+            join_and_apply(a, length);
+            if (a != b) join_and_apply(b, length);
+        }
+    }
+
+    template<typename T_range_op>
+    void process_range(int a, int b, bool needs_join, T_range_op &&range_op) {
+        if (a == b) return;
+        push_all(a, b);
+        int original_a = a, original_b = b;
+        int length = 1, r_size = 0;
+
+        for (a += tree_n, b += tree_n; a < b; a /= 2, b /= 2, length *= 2) {
+            if (a & 1)
+                range_op(a++, length);
+
+            if (b & 1)
+                right_half[r_size++] = {--b, length};
+        }
+
+        for (int i = r_size - 1; i >= 0; i--)
+            range_op(right_half[i].first, right_half[i].second);
+
+        if (needs_join)
+            join_all(original_a, original_b);
+    }
+
+    segment query(int a, int b) {
+        assert(0 <= a && a <= b && b <= tree_n);
+        segment answer;
+
+        process_range(a, b, false, [&](int position, int) {
+            answer.join(tree[position]);
+        });
+
+        return answer;
+    }
+
+    void update(int a, int b, const segment_change &change) {
+        assert(0 <= a && a <= b && b <= tree_n);
+
+        process_range(a, b, true, [&](int position, int length) {
+            apply_and_combine(position, length, change);
+        });
+    }
+
+    vector<segment> to_array() {
+        for (int i = 1; i < tree_n; i++)
+            push_down(i, tree_n >> highest_bit(i));
+
+        vector<segment> segs(tree_n);
+
+        for (int i = 0; i < tree_n; i++)
+            segs[i] = tree[tree_n + i];
+
+        return segs;
+    }
+
+    void update_single(int index, const segment &seg) {
         assert(0 <= index && index < tree_n);
-        T current = get(index);
- 
-        if (current == value)
-            return false;
- 
-        update(index, value - current);
-        return true;
+        int position = tree_n + index;
+
+        for (int up = highest_bit(tree_n); up > 0; up--)
+            push_down(position >> up, 1 << up);
+
+        tree[position] = seg;
+
+        while (position > 1) {
+            position /= 2;
+            tree[position].join(tree[2 * position], tree[2 * position + 1]);
+        }
     }
- 
-    // Returns the largest p in `[0, tree_n]` such that `query(p) <= sum`. Returns -1 if no such p exists (`sum < 0`).
-    // Can be used as an ordered set/multiset on indices in `[0, tree_n)` by using the tree as a 0/1 or frequency array:
-    // `set(index, 1)` is equivalent to insert(index). `update(index, +1)` is equivalent to multiset.insert(index).
-    // `set(index, 0)` or `update(index, -1)` are equivalent to erase(index).
-    // `get(index)` provides whether index is present or not, or the count of index (if multiset).
-    // `query(index)` provides the count of elements < index.
-    // `find_last_prefix(k)` finds the k-th smallest element (0-indexed). Returns `tree_n` for `sum >= set.size()`.
-    int find_last_prefix(T sum) const {
-        if (sum < 0)
-            return -1;
- 
-        int prefix = 0;
- 
-        for (int k = highest_bit(tree_n); k >= 0; k--)
-            if (prefix + (1 << k) <= tree_n && tree[prefix + (1 << k)] <= sum) {
-                prefix += 1 << k;
-                sum -= tree[prefix];
+
+    // Finds the last subarray starting at `first` that satisifes `should_join` via binary search in O(log n).
+    template<typename T_bool>
+    int find_last_subarray(T_bool &&should_join, int n, int first = 0) {
+        assert(0 <= first && first <= n);
+        segment current;
+
+        // Check the degenerate case.
+        if (!should_join(current, current))
+            return first - 1;
+
+        return y_combinator([&](auto search, int position, int start, int end) -> int {
+            if (end <= first) {
+                return end;
+            } else if (first <= start && end <= n && should_join(current, tree[position])) {
+                current.join(tree[position]);
+                return end;
+            } else if (end - start == 1) {
+                return start;
             }
- 
-        return prefix;
+
+            push_down(position, end - start);
+            int mid = (start + end) / 2;
+            int left = search(2 * position, start, mid);
+            return left < mid ? left : search(2 * position + 1, mid, end);
+        })(1, 0, tree_n);
     }
 };
 
@@ -137,7 +255,7 @@ void run_cases() {
 
     int64_t maxA = *max_element(A.begin(), A.end()) + 10;
 
-    fenwick_tree<int64_t> left_mod, right_mod;
+    segment_tree left_mod, right_mod;
     left_mod.init(maxA); right_mod.init(maxA); ;
 
     // left_mod stores current % old, right_mod stores old % current
@@ -146,15 +264,14 @@ void run_cases() {
     vector<int64_t> answers(N);
     for(int64_t i = 0; i < N; i++) {
         int64_t val = A[i];
-        int64_t left = i * val - left_mod.query(val + 1);
+        int64_t left = i * val - left_mod.query(val, val + 1).sum;
         int64_t right = prefix_sums;
-        for(int d = 1; d * val < maxA; d++) {
-            right -= d * val * right_mod.query(d * val, min(maxA, (d + 1) * val));
-            left_mod.update(d * val, d * val);
-            left_mod.update(min(maxA - 1, (d + 1) * val), - d * val);
+        for(int d = 1; d * val <= maxA; d++) {
+            right -= d * val * right_mod.query(d * val, min(maxA, (d + 1) * val)).sum;
+            left_mod.update(d * val, min(maxA, (d + 1) * val), segment_change(d * val));
         }
         prefix_sums += val;
-        right_mod.update(val, 1);
+        right_mod.update(val, val + 1, segment_change(1));
         answers[i] = (i - 1 >= 0 ? answers[i - 1] : 0) + left + right;
     }
 
